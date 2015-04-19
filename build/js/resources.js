5329aec2d6603eed433a6b14610b0fe5c75852c4
TDN78 <thedarknightdu78@gmail.com>
2015-04-19 16:55:50 -0700

    add built files

---
+++ build/js/resources.js
@@ -0,0 +1,117 @@
+game.resources = [
+    {
+        "name": "bonus",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "changecolor",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "cling",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "endoflevel",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "b0",
+        "type": "image",
+        "src": "data/img/b0.png"
+    },
+    {
+        "name": "b1",
+        "type": "image",
+        "src": "data/img/b1.png"
+    },
+    {
+        "name": "title_screen",
+        "type": "image",
+        "src": "data/img/gui/title_screen.png"
+    },
+    {
+        "name": "title_screen_press",
+        "type": "image",
+        "src": "data/img/gui/title_screen_press.png"
+    },
+    {
+        "name": "tiles",
+        "type": "image",
+        "src": "data/img/map/tiles.png"
+    },
+    {
+        "name": "blue_bonus",
+        "type": "image",
+        "src": "data/img/sprite/blue_bonus.png"
+    },
+    {
+        "name": "end_of_level",
+        "type": "image",
+        "src": "data/img/sprite/end_of_level.png"
+    },
+    {
+        "name": "first_bonus",
+        "type": "image",
+        "src": "data/img/sprite/first_bonus.png"
+    },
+    {
+        "name": "hero",
+        "type": "image",
+        "src": "data/img/sprite/hero.png"
+    },
+    {
+        "name": "red_bonus",
+        "type": "image",
+        "src": "data/img/sprite/red_bonus.png"
+    },
+    {
+        "name": "second_bonus",
+        "type": "image",
+        "src": "data/img/sprite/second_bonus.png"
+    },
+    {
+        "name": "yellow_bonus",
+        "type": "image",
+        "src": "data/img/sprite/yellow_bonus.png"
+    },
+    {
+        "name": "area0",
+        "type": "tmx",
+        "src": "data/map/area0.tmx"
+    },
+    {
+        "name": "area1",
+        "type": "tmx",
+        "src": "data/map/area1.tmx"
+    },
+    {
+        "name": "area2",
+        "type": "tmx",
+        "src": "data/map/area2.tmx"
+    },
+    {
+        "name": "area3",
+        "type": "tmx",
+        "src": "data/map/area3.tmx"
+    },
+    {
+        "name": "area4",
+        "type": "tmx",
+        "src": "data/map/area4.tmx"
+    },
+    {
+        "name": "area5",
+        "type": "tmx",
+        "src": "data/map/area5.tmx"
+    },
+    {
+        "name": "area6",
+        "type": "tmx",
+        "src": "data/map/area6.tmx"
+    }
+];
